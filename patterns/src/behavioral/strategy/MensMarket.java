f2f99f461f74691cf211b2331d9e41dbd1767db1
glebgalkin <gleb.galkin10@gmail.com>
2022-10-13 22:49:21 -0300

    Strategy

---
+++ patterns/src/behavioral/strategy/MensMarket.java
@@ -0,0 +1,8 @@
+package behavioral.strategy;
+
+public class MensMarket implements ClothesStrategy{
+    @Override
+    public String getSizeChart() {
+        return "42-46";
+    }
+}
---
+++ patterns/src/behavioral/strategy/WomenMarket.java
@@ -0,0 +1,8 @@
+package behavioral.strategy;
+
+public class WomenMarket implements ClothesStrategy{
+    @Override
+    public String getSizeChart() {
+        return "32-38";
+    }
+}
